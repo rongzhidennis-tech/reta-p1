f8755d7da82e6deb6d25c6109ff7438e6bce36ff
Dennis Chen <rongzhi.dennis@gmail.com>
2026-07-02 13:48:30 -0400

    Initial Commit

---
+++ RetaP1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetaP1
+//
+//  Created by RongzhiChen on 7/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetaP1/RetaP1App.swift
@@ -0,0 +1,17 @@
+//
+//  RetaP1App.swift
+//  RetaP1
+//
+//  Created by RongzhiChen on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RetaP1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
